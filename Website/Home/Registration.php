15ac06c74cb00be7bbc075e0935aaf54ef8a5ffc
Branden M <brandenmcneill21@gmail.com>
2023-07-05 15:39:21 -0700

    Removed music test

--- Website/Home/Registration.php
+++ Website/Home/Registration.php
@@ -105,7 +105,6 @@ body {
 </form>
             </nav>
         </header>
-        <iframe allow="autoplay *; encrypted-media *;" frameborder="0" height="450" style="width:100%;max-width:660px;overflow:hidden;background:transparent;" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="https://embed.music.apple.com/us/album/1596530747"></iframe>
 </div>
 </body>
 </html>
